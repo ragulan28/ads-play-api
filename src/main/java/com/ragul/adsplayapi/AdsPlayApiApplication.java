4c770b8c73c01870e0f28d2718513649a3290c26
Ragulan Gopalapillai <ragulan28@gmail.com>
2019-10-23 21:44:33 +0530

    Default admin added

--- src/main/java/com/ragul/adsplayapi/AdsPlayApiApplication.java
+++ src/main/java/com/ragul/adsplayapi/AdsPlayApiApplication.java
@@ -1,13 +1,27 @@
 package com.ragul.adsplayapi;
 
+import com.ragul.adsplayapi.Model.Admin;
+import com.ragul.adsplayapi.Repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdsPlayApiApplication {
+public class AdsPlayApiApplication implements CommandLineRunner {
+    @Autowired
+    AdminRepository adminRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(AdsPlayApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Admin admin=new Admin();
+        admin.setName("ragulan");
+        admin.setEmail("ragulan28@gmail.com");
+        admin.setPassword("ragulan28@gmail.com");
+        adminRepository.save(admin);
+    }
 }
